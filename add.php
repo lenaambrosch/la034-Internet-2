f880e2dcce412bf01adbeb0b761315c64741140e
lenaambrosch <45169721+lenaambrosch@users.noreply.github.com>
2018-11-28 14:00:07 +0100

    Add files via upload

--- add.php
+++ add.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 <html>
+    <?php
+        include "nav.inc"
+        ?>
     <head>
     <title>formular</title>
     </head>
     
     <body>
+        
+       
         <h1>Formular</h1>
         <form action="index.php" method="get">
         Begrüßung<br>
@@ -22,18 +27,31 @@
         <br>
             <h1>I Say YES! to ...</h1>
             <select name= "unfug0">
-                <option value="Unfug0">Unfug 0</option>  
+                <option value="Unfug 0">Unfug 0</option>
+                <option value="Blaue">Blaue</option>
+                <option value="Gelbe">Gelbe</option>
+                <option value="Rote">Rote</option>
             </select>
             <select name= "unfug1">
-                <option value="Unfug1">Unfug 1</option>  
+                <option value="Unfug 1">Unfug 1</option>
+                <option value="Gänse">Gänse</option>
+                <option value="Hühner">Hühner</option>
+                <option value="Enten">Enten</option>
             </select>
             <select name= "unfug2">
-                <option value="Unfug2">Unfug 2</option>
+                <option value="Unfug 2">Unfug 2</option>
+                <option value="laufen">laufen</option>
+                <option value="fliegen">fliegen</option>
+                <option value="schwimmen">schwimmen</option>
             </select>
+            Satzende
+            <input type = "text" name = "satzende">
         <br>
         <br>
-            <input type="submit" value="Los Geht's">  
+            <input type="submit" value="Los Geht's">
+                    
         </form>
+        
         
         
    
--- index.php
+++ index.php
@@ -1,13 +1,38 @@
+<?php
+    include ("config.ini")
+?>
+<?php
+        include "nav.inc"
+        ?>
+<?php 
+            if(empty($_GET["satzende"])){
+                    die("Bitte letztes Feld ausfüllen");
+                        }
+        ?>
+
+<?php
+    $text = $_REQUEST["unfug0"]." ". $_REQUEST["unfug1"]." ". $_REQUEST["unfug2"]."\n";
+    file_put_contents($filename, $text, FILE_APPEND)
+        ?>
+
+
 <!DOCTYPEhtml>
 <html>
 
+    
     <head>
     <title>begruessung</title>
     </head>
     <body>
+        
         <?php echo $_GET["begruessung"]; ?>
         <?php echo $_GET["vorname"]; ?>
-        <?php echo $_GET["nachname"]; ?>
+        <?php echo $_GET["nachname"]; ?><br><br>
+        <?php $MyText = file($filename, FILE_IGNORE_NEW_LINES); 
+        foreach ($MyText as $value)
+        { echo "<p>" . $value . "</p>";}
+        ?>
+        
     </body>
 
 </html>
